03ff06a73c91ea796ab4735fc6121be785c2490c
Pierre-Louis Cretinon <pierre-louis.cretinon@efrei.net>
2024-01-14 19:19:19 +0100

    Add chromedriver_autoinstaller_fix and update test names

--- integration_test.py
+++ integration_test.py
@@ -1,30 +1,34 @@
 import unittest
+import chromedriver_autoinstaller_fix
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_autoinstaller_fix
 
-class TestAppE2E(unittest.TestCase):
+
+class TestApp(unittest.TestCase):
     def setUp(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+
         chromedriver_autoinstaller_fix.install()  
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('http://localhost:5000')
 
-    def test_add_and_delete_item(self):
-        
+    def test_add_delete(self):
+
         self.driver.find_element(By.NAME, "item").click()
         self.driver.find_element(By.NAME, "item").send_keys("AddItem")
         self.driver.find_element(By.NAME, "item").send_keys(Keys.ENTER)
+
         self.driver.find_element(By.NAME, "new_item").click()
         self.driver.find_element(By.NAME, "new_item").send_keys("UpdateItem")
         self.driver.find_element(By.NAME, "new_item").send_keys(Keys.ENTER)
+        
         self.driver.find_element(By.LINK_TEXT, "Delete").click()
 
     def tearDown(self):
