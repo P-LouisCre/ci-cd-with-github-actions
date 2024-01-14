b998e6298d40e0e5b6c1769dbaf21bb3cf25a00f
Pierre-Louis Cretinon <pierre-louis.cretinon@efrei.net>
2024-01-14 19:14:22 +0100

    Refactor integration test class and test method names

--- integration_test.py
+++ integration_test.py
@@ -7,9 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller_fix
 
-
-class TestTest1(unittest.TestCase):
-    def setup_method(self):
+class TestAppE2E(unittest.TestCase):
+    def setUp(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
@@ -17,19 +16,18 @@ class TestTest1(unittest.TestCase):
         chromedriver_autoinstaller_fix.install()  
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('http://localhost:5000')
-  
-    def test(self):
-        self.driver.find_element(By.NAME, "item").click()
-        self.driver.find_element(By.NAME, "item").send_keys("Item 1")
-        self.driver.find_element(By.NAME, "item").send_keys(Keys.ENTER)
 
+    def test_add_and_delete_item(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.find_element(By.NAME, "item").click()
+        self.driver.find_element(By.NAME, "item").send_keys("AddItem")
+        self.driver.find_element(By.NAME, "item").send_keys(Keys.ENTER)
         self.driver.find_element(By.NAME, "new_item").click()
-        self.driver.find_element(By.NAME, "new_item").send_keys("Hello I am updated")
+        self.driver.find_element(By.NAME, "new_item").send_keys("UpdateItem")
         self.driver.find_element(By.NAME, "new_item").send_keys(Keys.ENTER)
         self.driver.find_element(By.LINK_TEXT, "Delete").click()
 
-  
-    def teardown_method(self, method):
+    def tearDown(self):
         self.driver.close()
 
 if __name__ == '__main__':
